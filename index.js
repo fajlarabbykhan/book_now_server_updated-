199d9c09c4cfe0d3a27435d141cb0bd95ac816cf
Fajla Rabby Khan <fajlarabbyk55@gmail.com>
2023-01-02 11:10:44 +0600

    connect with mongodb and check error

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 5000;
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+mongoose.set("strictQuery", false);
+//middlewares
+app.use(cors());
+app.use(express.json());
+
+//connect with mongodb
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB);
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnected", () => {
+  console.log("Mongodb disconnected");
+});
+mongoose.connection.on("connected", () => {
+  console.log("Mongodb connected");
+});
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("This is book now app server root");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  connectDB();
+  console.log(`Book Now app listening on port ${port}`);
 });
