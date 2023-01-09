7f93dbcf2c0e8d8a6f9472a17089b08b816a2272
Fajla Rabby Khan <fajlarabbyk55@gmail.com>
2023-01-09 20:36:53 +0600

    create hotel rooms in a hotel

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,5 +1,25 @@
 import express from "express";
+import {
+  createRoom,
+  updateRoom,
+  deleteRoom,
+  getRoom,
+  getRooms,
+} from "../controllers/room.js";
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
+
+//create rooms
+router.post("/:hotelid", verifyAdmin, createRoom);
+//update rooms
+router.put("/:id", verifyAdmin, updateRoom);
+//delete roomss
+router.delete("/:id/:hotelid", verifyAdmin, deleteRoom);
+//get a specefic room
+router.get("/:id", getRoom);
+
+//get all room
+router.get("/", getRooms);
 
 export default router;
