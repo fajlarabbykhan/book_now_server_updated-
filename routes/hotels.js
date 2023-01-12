de63f0d6315ba3546bb5aed526f9d865d3aff760
Fajla Rabby Khan <fajlarabbyk55@gmail.com>
2023-01-12 12:16:27 +0600

    count properties for every city

--- routes/hotels.js
+++ routes/hotels.js
@@ -5,6 +5,7 @@ import {
   deleteHotel,
   getHotel,
   getHotels,
+  countByCity,
 } from "../controllers/hotel.js";
 import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
 
@@ -15,11 +16,13 @@ router.post("/", verifyAdmin, createHotel);
 //update hotels
 router.put("/:id", verifyAdmin, updateHotel);
 //delete hotels
-router.delete("/:id", verifyAdmin, deleteHotel);
+router.delete("/find/:id", verifyAdmin, deleteHotel);
 //get a specefic hotel
 router.get("/:id", getHotel);
 
 //get all hotels
 router.get("/", getHotels);
+router.get("/countByCity", countByCity);
+router.get("/countByType", getHotels);
 
 export default router;
